81dc2d0918a5f4deb58d6917adb054d6bc9ee731
vsinhabbsr <eee.varun@gmail.com>
2022-06-23 08:54:36 +0530

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+//Program to print Hello World
+
+class HelloWorld 
+{ //start of class
+
+   public static void main(String[] args) 
+   
+    { //start of main() method
+    
+        System.out.println("Hello, World!");//printing Hello World
+        
+    } //end of main() method
+    
+} //end of the class
